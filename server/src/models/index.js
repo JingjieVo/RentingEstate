5fe8c5f552267ad162a79384277b59250ba6be94
LeVuXuanAn <levuxuanan.work@gmail.com>
2024-11-21 21:03:51 +0700

    Add new feature mul-img

---
+++ server/src/models/index.js
@@ -0,0 +1,24 @@
+// src/models/index.js
+import Post from './post.js';
+import Province from './province.js';
+import Label from './label.js';
+import Price from './price.js';
+import Area from './area.js';
+import Attribute from './attribute.js';
+import Category from './category.js';
+import Image from './image.js';
+import Overview from './overview.js';
+import User from './user.js';
+
+export default {
+  Post,
+  Province,
+  Label,
+  Price,
+  Area,
+  Attribute,
+  Category,
+  Image,
+  Overview,
+  User
+};
